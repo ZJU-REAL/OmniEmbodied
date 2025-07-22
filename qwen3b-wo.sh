6631440f44efb42f039490d56562253e3c9ebd86
ZixuanWang <wangzixuan1210@gmail.com>
2025-07-22 20:14:28 +0800

    update script

---
+++ qwen3b-wo.sh
@@ -0,0 +1,13 @@
+python examples/single_agent_example.py \
+    --model qwen3b \
+    --observation-mode explore \
+    --suffix qwen3b_single_test \
+    --parallel \
+    --config-override "parallel_evaluation.scenario_parallelism.max_parallel_scenarios=76"
+
+python examples/centralized_agent_example.py \
+    --model qwen7b \
+    --observation-mode explore \
+    --suffix qwen3b_centralized_test \
+    --parallel \
+    --config-override "parallel_evaluation.scenario_parallelism.max_parallel_scenarios=76"
